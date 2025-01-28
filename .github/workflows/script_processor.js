e750912c20a390b716841508d7bec1a7160e45c1
halon <45232009+jLn0n@users.noreply.github.com>
2025-01-28 21:12:23 +0800

    javascript sucks ass why the fuck am i still using this

--- .github/workflows/script_processor.js
+++ .github/workflows/script_processor.js
@@ -12,7 +12,7 @@ module.exports = function(github, context, core) {
 			}
 		}
 
-		github.rest.gists.request(`PATCH /gists/${gist_id}`, {
+		github.rest.request(`PATCH /gists/${gist_id}`, {
 			gist_id: gist_id,
 			description: `${script_dir} (https://github.com/${context.repo.owner}/${context.repo.repo}/tree/${context.sha}) - processed`,
 			files: files,
