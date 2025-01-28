aa071ba50eee445d6292825b28b28c814d4c17a5
halon <45232009+jLn0n@users.noreply.github.com>
2025-01-28 19:32:59 +0800

    wotdafak man where do i get dir of scripts

---
+++ .github/workflows/script_processor.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+const child_proc = require("child_process");
+
+const process_script_dir = (script_dir) => {
+  const output_dirs = [];
+  let scripts_name;
+
+  const config_file_dir = path.join(script_dir, "config.json");
+  if (!fs.existsSync(config_file_dir)) {
+    console.log(`No config.json exists in directory: '${script_dir}'`)
+    return;
+  }
+
+  const config_data = JSON.parse(fs.readFileSync(config_file_dir, 'utf8')) || {};
+  scripts_name = config_data.name;
+
+  if (typeof scripts_name !== "string") {
+    console.log(`Name not provided on '${config_file_dir}', skipping...`);
+    return;
+  }
+
+  console.log(`Processing scripts '${scripts_name}' on directory: '${script_dir}'`);
+
+  if (typeof config_data.darklua_config !== "string") {
+    console.log("Darklua config file not specified (or not string), skipping...");
+    return;
+  }
+  if (config_data.process_files.length < 1) {
+    console.log("There are no files to process, skipping...");
+    return;
+  }
+
+  const full_darklua_config_dir = path.join(script_dir, config_data.darklua_config);
+
+  if (fs.existsSync(full_darklua_config_dir)) {
+    let process_count = 0
+
+    for (let file_dir of config_data.process_files) {
+      const full_file_dir = path.join(script_dir, file_dir);
+      if (!fs.existsSync(full_file_dir)) {
+        console.log(`File '${file_dir}' from directory '${script_dir}' not found, skipping...`)
+        continue;
+      }
+
+      const output_dir = path.join("output", file_dir);
+      const command = `./darklua process ${full_file_dir} ${output_dir} -c ${full_darklua_config_dir}`;
+
+      child_proc.exec(command, (cmd_err, stdout, stderr) => {
+        if (cmd_err) {
+          console.error(`Darklua errored with file '${file_dir}':`, cmd_err);
+        } else {
+          process_count += 1;
+          console.log(`Darklua processing succeed, file is now saved at '${output_dir}'`);
+          output_dirs.push(output_dir)
+        }
+      });
+    }
+
+    console.log(`[${process_count}/${config_data.process_files.length}] files processed.`);
+  } else {
+    console.log("Darklua config file not found, skipping...")
+    return;
+  }
+  return scripts_name, output_dirs;
+}
+
+
+module.exports = function(github, context, core) {
+  console.log(github, context, core)
+  const branch_dir = context.matrix.directory;
+  const scripts_dir = path.join(branch_dir, "scripts");
+
+  const _output = []
+
+  for (let dir of fs.readdirSync(scripts_dir)) {
+    const [scripts_name, output_dirs] = process_script_dir(path.join(scripts_dir, dir));
+    if (typeof scripts_name !== "string") {
+      continue;
+    }
+  
+    _output = _output.concat(output_dirs)
+  }
+
+  core.setOutput('scripts_dirs', JSON.stringify(_output));
+};
